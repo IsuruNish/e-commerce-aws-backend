9159ca17e2b5d35218bfd724959301351ccef5b2
Isuru Nishadha <isurunishadha99@gmail.com>
2024-08-02 23:23:14 +0530

    Added micro services

---
+++ buyer-module/src/main/java/com/ecommerce/buyer_module/BuyerModuleApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.buyer_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyerModuleApplication.class, args);
+	}
+
+}
---
+++ delivery-module/src/main/java/com/ecommerce/delivery_module/DeliveryModuleApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.delivery_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryModuleApplication.class, args);
+	}
+
+}
