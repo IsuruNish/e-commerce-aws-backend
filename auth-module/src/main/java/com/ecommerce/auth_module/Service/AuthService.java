05f249091c2b493a108ceaae1550713c66ab8bac
Isuru Nishadha <isurunishadha99@gmail.com>
2024-08-04 20:28:40 +0530

    Added the login and signup functionality

---
+++ auth-module/src/main/java/com/ecommerce/auth_module/Service/AuthService.java
@@ -0,0 +1,49 @@
+package com.ecommerce.auth_module.Service;
+
+import com.ecommerce.auth_module.Dto.LoginUserDto;
+import com.ecommerce.auth_module.Dto.RegisterUserDto;
+import com.ecommerce.auth_module.Model.User;
+import com.ecommerce.auth_module.Repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = new User(input.getEmail(), passwordEncoder.encode(input.getPassword()), input.getName(), input.getRole());
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        User user = userRepository.findByEmail(input.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!user.isEnabled()) {
+            throw new RuntimeException("Account not verified. Please verify your account.");
+        }
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return user;
+    }
+}
