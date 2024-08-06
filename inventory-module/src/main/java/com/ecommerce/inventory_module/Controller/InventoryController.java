cfff3d8c73da78ca09a91b7b717928407310d53c
Isuru Nishadha <isurunishadha99@gmail.com>
2024-08-06 22:34:10 +0530

    Added product related functionality

--- inventory-module/src/main/java/com/ecommerce/inventory_module/Controller/InventoryController.java
+++ inventory-module/src/main/java/com/ecommerce/inventory_module/Controller/InventoryController.java
@@ -1,2 +1,60 @@
-package com.ecommerce.inventory_module.Controller;public class InventoryController {
+package com.ecommerce.inventory_module.Controller;
+
+import com.ecommerce.inventory_module.Dao.ProductDto;
+import com.ecommerce.inventory_module.Model.Product;
+import com.ecommerce.inventory_module.Service.InventoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products = inventoryService.getProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Product>> getAllProductsByUserId(@PathVariable("id") int userId) {
+        List<Product> products = inventoryService.getProductByUserId(userId);
+        return ResponseEntity.ok(products);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Product> addNewProduct(@RequestBody ProductDto productDto) {
+        Product product = inventoryService.addProduct(productDto);
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@RequestBody ProductDto productDto, @PathVariable("id") int productId) {
+        Product product = inventoryService.updateProduct(productDto, productId);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable("id") int productId) {
+        inventoryService.deleteProduct(productId);
+        return ResponseEntity.ok("Product with the ID " + productId + " is deleted");
+    }
+
+
+    //todo: do this
+    @PostMapping("/checkout")
+    public ResponseEntity<Product> updateInventory(@RequestBody ProductDto productDto) {
+//        Product product = inventoryService.updateProduct(productDto);
+//        return ResponseEntity.ok(product);
+    }
+
 }
--- inventory-module/src/main/java/com/ecommerce/inventory_module/Repository/InventoryRepository.java
+++ inventory-module/src/main/java/com/ecommerce/inventory_module/Repository/InventoryRepository.java
@@ -1,2 +1,12 @@
-package com.ecommerce.inventory_module.Repository;public interface InventoryRepository {
+package com.ecommerce.inventory_module.Repository;
+
+import com.ecommerce.inventory_module.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Product, Integer> {
+    ArrayList<Product> findByUserId(int userId);
 }
