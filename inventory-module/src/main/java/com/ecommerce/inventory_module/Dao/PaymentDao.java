13353d5abdf1279ee6066fadd88aabbe75cd12ac
Isuru Nishadha <isurunishadha99@gmail.com>
2024-08-09 22:53:15 +0530

    Deleted delivery-module

--- inventory-module/src/main/java/com/ecommerce/inventory_module/Dao/PaymentDao.java
+++ inventory-module/src/main/java/com/ecommerce/inventory_module/Dao/PaymentDao.java
@@ -1,2 +1,16 @@
-package com.ecommerce.inventory_module.Dao;public class PaymentDao {
+package com.ecommerce.inventory_module.Dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDao {
+    private String cardName;
+    private String cardNumber;
+    private String ExpDate;
+    private int CVN;
+    private float Fee;
 }
